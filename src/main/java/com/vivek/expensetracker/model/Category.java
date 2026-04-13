2dce70a97df6852e30168a94adeb14f8539b3a04
Vivek Shelke <vivekshelke12jul@gmail.com>
2026-04-13 12:26:08 +0530

    expense model created

---
+++ src/main/java/com/vivek/expensetracker/model/Category.java
@@ -0,0 +1,14 @@
+package com.vivek.expensetracker.model;
+
+public enum Category {
+    GENERAL,
+    FOOD,
+    RENT,
+    TRAVEL,
+    ENTERTAINMENT,
+    SHOPPING,
+    HEALTH,
+    INVESTMENT,
+    EDUCATION,
+    TAXES
+}
---
+++ src/main/java/com/vivek/expensetracker/model/Expense.java
@@ -0,0 +1,31 @@
+package com.vivek.expensetracker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
